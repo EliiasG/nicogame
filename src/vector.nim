e9064b8900f57b46c37cb8867b1366939ec29c3b
EliiasG <egrundtvig@gmail.com>
2022-11-11 15:52:35 +0100

    added vector.nim

--- src/vector.nim
+++ src/vector.nim
@@ -1,4 +1,29 @@
+import math
 type
-    vector = object
-        x: float
-        y: float
+    Vector* = object
+        x*: float
+        y*: float
+
+proc newVector*(x, y: float): Vector =
+    result.x = x
+    result.y = y
+
+proc `$`*(vec: Vector): string =
+    result = $vec.x & ", " & $vec.y
+
+proc `+`*(vec0, vec1: Vector): Vector =
+    newVector(vec0.x + vec1.x, vec0.y + vec1.y)
+
+proc `+=`*(vec0: var Vector, vec1: Vector) =
+    vec0 = vec0 + vec1
+
+proc angle*(vec: Vector): float =
+    arctan2(vec.y, vec.x)
+
+proc normalized(vec: Vector): Vector =
+    let ang: float = vec.angle
+    newVector(cos(ang), sin(ang))
+
+var vec: Vector = newVector(1, 1)
+#vec += newVector(1, 1)
+echo vec.normalized
